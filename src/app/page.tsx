a42ac1c8eaa6512f2ca274f314763faa02160cac
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-05-01 15:39:59 -0400

    link database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,18 @@ import { db } from "@/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/68174d15-fb21-47c3-a006-96c294302363-tsfq0a.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
   return (
     <main>
-      {images.map((image) => (
-        <div key={image.id}>{image.name}</div>
-      ))}
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image, index) => (
+        {images.map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
